19bb4154ccab7cc59f027f8727c32675652be6d6
Jay Singh Dhakad <jaysingh20196@gmail.com>
2025-09-18 14:09:48 +0530

    feat: refactored config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,7 +8,8 @@ const config: HardhatUserConfig = {
   namedAccounts: {
     deployer: {
       default: 0,
-      base: `${process.env.DEPLOYER}`
+      base: `${process.env.DEPLOYER}`,
+      bsc: `${process.env.DEPLOYER}`
     },
     user1: {
       default: 1
@@ -16,10 +17,10 @@ const config: HardhatUserConfig = {
     user2: {
       default: 2
     }
-  }, 
+  },
   networks: {
     base: {
-      url: process.env.BSC_RPC || "",
+      url: process.env.BASE_RPC || "https://mainnet.base.org",
       chainId: 8453,
       saveDeployments: true,
       accounts: {
@@ -32,11 +33,26 @@ const config: HardhatUserConfig = {
           apiKey: process.env.ETHERSCAN_KEY
         }
       },
+    },
+    bsc: {
+      url: process.env.BSC_RPC || "https://bsc-dataseed1.binance.org",
+      chainId: 56,
+      accounts: {
+        mnemonic: `${process.env.MEMONIC}`,
+      },
+      gas: 30_000_000,
+      verify: {
+        etherscan: {
+          apiUrl: "https://api.bscscan.com",
+          apiKey: process.env.BSCSCAN_KEY || ""
+        }
+      }
     }
   },
   etherscan: {
     apiKey: {
       base: `${process.env.ETHERSCAN_KEY}`,
+      bsc: `${process.env.BSCSCAN_KEY}`,
     },
   },
   sourcify: {
